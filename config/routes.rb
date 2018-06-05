b716b70761f7324d269f7f90f760e95cf65b1ce3
ritik02 <ritikxman@gmail.com>
2018-06-05 18:51:36 +0530

    Update Project View

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root 'users#project'
   get 'users/project'
   get 'users/home' => 'users#home'
-  get 'users/job' => 'users#job'
+  get 'users/commit' => 'users#job'
   patch 'users/home', :to => 'users#update', :as => :user
   devise_for :users, contollers: {cas_sessions: 'sessions'}
-
 end
