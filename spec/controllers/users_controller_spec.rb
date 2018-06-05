c20832bdf0a63beeb56fc6f7c980fc2e6f2e6ac0
archit.j.aux <archit.j.aux@go-jek.com>
2018-06-05 16:56:50 +0530

    add controller rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+	fixtures :users
 
+	describe "GET users#home" do
+		it "should redirect signed in user to the home page for updating token" do
+			sign_in users(:one)
+			get :home
+			expect(assigns(:user)).to eq(users(:one))
+		end
+
+		it "should show projects page if gitlab token is valid" do
+			sign_in users(:one)
+			form_params = {
+											user: {
+												gitlab_token: decrypt_access_token(users(:one).gitlab_token)
+											}
+ 									 }
+  		patch :update, params: form_params
+  		expect(response).to redirect_to(users_project_url)
+		end
+
+		it "should reload home page if gitlab token is invalid" do
+			sign_in users(:one)
+			form_params = {
+											user: {
+												gitlab_token: "invalid_gitlab_token"
+											}
+ 									 }
+  		patch :update, params: form_params
+  		expect(response).to redirect_to(users_home_url)
+		end
+	end
+
+	describe "PATCH users#update" do
+		it "should update gitlab token for a user if valid" do
+			new_user = users(:three)
+			sign_in new_user
+			expected_token = users(:one).gitlab_token
+			form_params = {
+											user: {
+												gitlab_token: decrypt_access_token(users(:one).gitlab_token)
+											}
+ 									 }
+  		patch :update, params: form_params
+  		expect(response).to redirect_to(users_project_url)
+  		#expect(new_user.gitlab_token).to eq(expected_token)
+		end
+
+		it "should not update gitlab token for a user if invalid" do
+			new_user = users(:three)
+			sign_in new_user
+			expected_token = users(:one).gitlab_token
+			form_params = {
+											user: {
+												gitlab_token: "invalid_gitlab_token"
+											}
+ 									 }
+  		patch :update, params: form_params
+  		expect(new_user.gitlab_token).to eq(nil)
+		end
+
+	end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,11 +5,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
+require 'users_helper'
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include UsersHelper
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
