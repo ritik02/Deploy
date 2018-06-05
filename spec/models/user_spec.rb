abf71220763d0acf16dd60c109571ce629d66b66
archit.j.aux <archit.j.aux@go-jek.com>
2018-06-05 15:06:20 +0530

    Add user model rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+	it "is valid with valid username" do
+		expect(User.new({username: "Testusername"})).to be_valid
+	end
+  it "is not valid without a title" do
+  	expect(User.new).to_not be_valid
+  end
 end
