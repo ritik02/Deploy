7b598f86bfa381b2d7e8f52024ededbe9d0b733f
Prakash Dwivedi <prakashdwivedi@Prakashs-MacBook-Pro.local>
2018-06-05 18:41:04 +0530

    Tests for services

---
+++ spec/services/gitlab_api_services_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+RSpec.describe GitlabApiServices  do
+	include UsersHelper
+	fixtures :users
+
+	context "check api for valid token " do
+		it "Should return TRUE when token is Valid'" do 
+         actual = GitlabApiServices.new(decrypt_access_token(users(:one).gitlab_token)).check_api_for_valid_token? 
+         expect(actual).to eq true
+     	end
+
+     	it "Should return FALSE when token is Invalid'" do 
+         actual = GitlabApiServices.new(decrypt_access_token(users(:two).gitlab_token)).check_api_for_valid_token? 
+         expect(actual).to eq false
+     	end
+	end 
+
+	context "check api for user details" do
+		it "Should contain the name of the user'" do 
+         actual = GitlabApiServices.new(decrypt_access_token(users(:one).gitlab_token)).get_user_details(users(:one).username)
+         expect(actual.first["name"]).to eq "Ritik Verma"
+     	end
+	end 
+
+	context "check api for number of pages" do
+		it "Should return 1 when there are 2 projects" do 
+         actual = GitlabApiServices.new(decrypt_access_token(users(:one).gitlab_token)).get_number_of_pages(users(:one).gitlab_userid)
+         expect(actual).to eq 1
+     	end
+	end 
+
+	context "check api for project details" do
+		it "Should return 2 projects" do 
+         actual = GitlabApiServices.new(decrypt_access_token(users(:one).gitlab_token)).get_user_projects(users(:one).gitlab_userid, 1)
+         expect(actual.length).to eq 2
+         expect(actual[0]["name"]).to eq "test2"
+     	end
+	end 
+
+	context "check api for search project results" do
+		it "Should return 'test2' project" do 
+         actual = GitlabApiServices.new(decrypt_access_token(users(:one).gitlab_token)).get_search_results(users(:one).gitlab_userid, "test2")
+         expect(actual[0]["name"]).to eq "test2"
+     	end
+	end 
+
+
+end
