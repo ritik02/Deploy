7b558dee53259d46e289fcade010ccba4ec75114
ritik02 <ritikxman@gmail.com>
2018-06-06 17:20:01 +0530

    Update devise configurations

---
+++ config/initializers/devise.rb
@@ -0,0 +1,11 @@
+Devise.setup do |config|
+  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.cas_base_url = Figaro.env.cas_base_url!
+  config.cas_login_url = Figaro.env.cas_login_url!
+  require 'devise/orm/active_record'
+  config.skip_session_storage = [:http_auth]
+  config.reconfirmable = true
+  config.stretches = Rails.env.test? ? 1 : 11
+  config.expire_all_remember_me_on_sign_out = true
+  config.sign_out_via = :delete
+end
