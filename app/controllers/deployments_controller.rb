c8a52cf10a367f289d76e6007621daa4921f9aff
Prakash Dwivedi <prakashdwivedi@Prakashs-MacBook-Pro.local>
2018-06-14 11:52:47 +0530

    add deployment link for the reviewer

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -1,7 +1,8 @@
 class DeploymentsController < ApplicationController
 
 def new
-	@deployment = Deployment.new({user_id: params[:user_id], project_name: params[:project_name], commit_id: params[:commit_id], status: "Created", diff_link: request.original_url})
+	git_diff_link = generate_diff_link(params)
+	@deployment = Deployment.new({user_id: params[:user_id], project_name: params[:project_name], commit_id: params[:commit_id], status: "Created", diff_link: git_diff_link})
 	@deployment.save
 	@user = current_user
 end
@@ -11,6 +12,7 @@ def create
 	return if !reviewer_not_valid?
   checklist_json_string = params[:deployments].to_json
   deployment.update({checklist: checklist_json_string, status: "Checklist Filled", reviewer_id: User.where(:email => params[:deployments][:reviewer_email]).first.id })
+	UserMailer.sample_email(current_user , @deployment).deliver
 	redirect_to deployments_path(:user_id => current_user.id)
 end
 
@@ -35,19 +37,16 @@ end
 private
 
 def reviewer_not_valid?
-	if User.where(:email => params[:deployments][:reviewer_email]).blank?
-		render 'layouts/error'
-		return false
-	end
-	return true
-end
-
-def reviewer_not_valid?
   if User.where(:email => params[:deployments][:reviewer_email]).blank?
     render 'layouts/error'
     return false
   end
   return true
 end
+
+def generate_diff_link(params)
+        git_diff_link =  "http://localhost:3000/users/"+ params[:user_id] + "/projects/" + params[:project_id] + "/commits/" + params[:commit_id] + "?last_deployed_commit=" + params[:last_deployed_commit] + "&project_name=" + params[:project_name]
+        git_diff_link
+    end
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,8 @@
 class UserMailer < ApplicationMailer
 	default from: 'notifications@example.com'
  
-  def welcome_email
-    # @user = params[:user]
-    # @url  = 'http://example.com/login'
-    mail(to: "prakash.d.aux@go-jek.com", subject: 'Welcome to My Awesome Site')
-  end
-
-  def sample_email(user)
+  def sample_email(user , deployment)
+    @deployment = deployment
     @user = user
     mail(to: "prakash.d.aux@go-jek.com" , subject: 'Sample Email')
   end
