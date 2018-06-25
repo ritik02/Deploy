cfddaaab14c42e7acb8c41a5aed18ef5c5e37de9
archit.j.aux <archit.j.aux@go-jek.com>
2018-06-25 14:28:55 +0530

    Add project deployments on project show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,7 @@ class ProjectsController < ApplicationController
     project_id = params["id"]
     get_gitlab_api_services(decrypt_access_token(current_user.gitlab_token))
     return if !project_id_valid?(project_id) || !pipeline_exist?(project_id)
+    @project_deployments = Deployment.where(project_id: project_id)
     jobs = @gitlab_api_services.get_jobs_of_a_pipeline(project_id, @pipeline["id"])
     map_stages_with_jobs(jobs)
   end
