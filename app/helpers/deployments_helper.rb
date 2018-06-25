2de7d5673e0c5f8bb404c5dd8e7918683efaed7d
archit.j.aux <archit.j.aux@go-jek.com>
2018-06-25 11:26:01 +0530

    modify deployment helpers generate diff link method

--- app/helpers/deployments_helper.rb
+++ app/helpers/deployments_helper.rb
@@ -1,5 +1,6 @@
 module DeploymentsHelper
 	def generate_user_specific_diff_link(diff_link, user_id)
-		diff_link[0..diff_link.index("users/")+5] + user_id.to_s + diff_link[diff_link.index("/projects/")...diff_link.length]
+		Figaro.env.diff_base_url + 
+		("/users/") + user_id.to_s + diff_link[diff_link.index("/projects/")...diff_link.length]
 	end
 end
