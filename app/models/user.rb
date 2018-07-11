bedc4d27b7b7fc05673de75bc1964eb2f8c8c7e1
archit2693 <archit2693.aj@gmail.com>
2018-07-11 12:04:59 +0530

    removed fetching of extra attributes from cas

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,4 @@
 class User < ApplicationRecord
   devise :cas_authenticatable
   validates :username , presence: true
-
-  def cas_extra_attributes=(extra_attributes)
-    extra_attributes.each do |name, value|
-    	puts name +  " " + value.to_s
-      case name.to_sym
-      when :name
-        self.name = value
-      when :email
-        self.email = value
-      end
-    end
-  end
 end
